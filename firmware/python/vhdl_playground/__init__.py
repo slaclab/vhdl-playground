8a532fb31034b3a87560c3d6c8a57fb2e4fac104
Christos Bakalis <cbakalis@slac.stanford.edu>
2025-05-13 12:48:43 -0700

    add misc python files

---
+++ firmware/python/vhdl_playground/__init__.py
@@ -0,0 +1,11 @@
+#-----------------------------------------------------------------------------
+# This file is part of the 'vhdl-playground'. It is subject to
+# the license terms in the LICENSE.txt file found in the top-level directory
+# of this distribution and at:
+#    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
+# No part of the 'vhdl-playground', including this file, may be
+# copied, modified, propagated, or distributed except according to the terms
+# contained in the LICENSE.txt file.
+#-----------------------------------------------------------------------------
+
+from vhdl_playground._App import *
---
+++ software/scripts/setupLibPaths.py
@@ -0,0 +1,20 @@
+#-----------------------------------------------------------------------------
+# This file is part of the 'vhdl-playground'. It is subject to
+# the license terms in the LICENSE.txt file found in the top-level directory
+# of this distribution and at:
+#    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
+# No part of the 'vhdl-playground', including this file, may be
+# copied, modified, propagated, or distributed except according to the terms
+# contained in the LICENSE.txt file.
+#-----------------------------------------------------------------------------
+
+# uncomment the following only if you have rogue installed in your environment
+
+# import pyrogue as pr
+# import os
+
+# top_level = os.path.realpath(__file__).split('software')[0]
+
+# pr.addLibraryPath(top_level+'firmware/submodules/surf/python')
+# pr.addLibraryPath(top_level+'firmware/submodules')
+# pr.addLibraryPath(top_level+'firmware/python')
